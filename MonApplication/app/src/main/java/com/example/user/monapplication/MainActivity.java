23471b0d3e1a6c6fbe4be84981c5dcca28a90624
HAZWR <hajar.bouhallaf@gmail.com>
2018-11-11 16:03:01 +0100

    myapplication

--- MonApplication/app/src/main/java/com/example/user/monapplication/MainActivity.java
+++ MonApplication/app/src/main/java/com/example/user/monapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.user.monapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private String[] drawerItemsList;
@@ -16,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         myDrawer = (ListView) findViewById(R.id.my_drawer);
         myDrawer.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_item, drawerItemsList));
+        myDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
